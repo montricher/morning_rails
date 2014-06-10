5b8dbf2c395b00b50d4d00cd988755fa76199777
jpark1120 <parkj1120@gmail.com>
2014-06-10 09:45:43 -0400

    made strong robot params in robots controller and views folder for robots

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -6,19 +6,38 @@ class RobotsController < ApplicationsController
   def new
   end
 
+
   def show
   end
 
   def create
   end
 
+
+# Joe's duty
   def edit
+
+    @robot = robot.find(robot_attributes[:id])
+
   end
 
   def update
+
+
+
   end
+
 
   def destroy
   end
+
+  private
+
+  def robot_attributes
+
+    params.require(:robot).permit(:name, :purpose, :price, :year)
+
+  end
+
 
 end
