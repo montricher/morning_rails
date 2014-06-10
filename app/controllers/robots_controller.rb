d155a23c6fc9864d5d7354197f118dbf4f01ba39
montricher <montricher@icloud.com>
2014-06-10 09:35:32 -0400

    Initial files setup

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,24 @@
+class RobotsController < ApplicationsController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,18 +1,5 @@
 class Robot < ActiveRecord::Base
 
-  def index
-  end
 
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 
 end
