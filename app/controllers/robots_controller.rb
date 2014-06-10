1d9f58d385c878aa16003b3790bd3ae42ac644ce
montricher <montricher@icloud.com>
2014-06-10 09:56:09 -0400

    Show and destroy methods

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -6,7 +6,12 @@ class RobotsController < ApplicationsController
   def new
   end
 
+  # David
   def show
+    # Find ID and render it
+    @robot = Robot.find(params[:id])
+    # @robot = Robot.new
+
   end
 
   def create
@@ -18,7 +23,16 @@ class RobotsController < ApplicationsController
   def update
   end
 
+  # David
   def destroy
+    # Find Robot ID and execute .destroy
+    @robot = Robot.find(params[:id])
+    @robot = robot.id
+    robot.destroy
+    # Need to redirect somewhere. Root_path?
+    redirect_to "robots#index"
+    # redirect_to root_path
   end
 
 end
+
